d26e2f7e4c3403fc179d7386ae7dbbc5e9d289be
Mario Egocheaga <work.mario.egocheaga@gmail.com>
2021-02-22 16:01:09 -0500

    Unity

--- GDD-211_QUIZ-2/Assets/Name.cs
+++ GDD-211_QUIZ-2/Assets/Name.cs
@@ -18,10 +18,21 @@ public class Name : MonoBehaviour
 
     void Awake()
     {
+        Color32[] Colors;
+
+
+        Colors = new Color32[4];
+        Colors[0] = new Color32(185, 0, 185, 255);
+        Colors[1] = new Color32(0, 127, 255, 255);
+        Colors[2] = new Color32(0, 0, 255, 255);
+        Colors[3] = new Color32(0, 255, 255, 255);
+
         playerName2 = PlayerPrefs.GetString("ThisPlayer", "playerName");
         playerLegal = PlayerPrefs.GetInt("ThisAge");
 
         tellPlayer.text = "Hi " + playerName2 + " your age is " + playerLegal;
+
+        Moose.transform.localScale = new Vector2(1, 1);
     }
 
     private void Update()
